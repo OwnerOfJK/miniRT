047610bc4f0e20dd1642a05e7faa529c89ca3218
elecarlier <eleonorecarlier@hotmail.be>
2024-07-08 21:33:21 +0200

    spo

--- src/utils/light/normal.c
+++ src/utils/light/normal.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   light_utils_0.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkaller <jkaller@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ecarlier <ecarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 11:53:17 by jkaller           #+#    #+#             */
-/*   Updated: 2024/06/21 12:31:53 by jkaller          ###   ########.fr       */
+/*   Updated: 2024/07/08 21:08:17 by ecarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,22 +45,39 @@ t_vector	compute_plane(t_object	*object)
 
 t_vector	compute_cylinder(t_object	*object, t_vector world_point)
 {
-	double		**inverse_transform;
-	t_vector	cylinder_base_to_point;
-	t_vector	world_normal;
-	t_vector	object_point;
+	t_vector	pc;
 	t_vector	normal;
+	t_vector	object_point;
+	double		**inverse_transform;
 
-	inverse_transform = m_inverse(object->transformation_matrix);
+	inverse_transform = m_inverse(object->transformation_matrix); //doesn't change anything
 	object_point = mv_mult(inverse_transform, world_point);
-	cylinder_base_to_point = v_sub(object_point,
-			v_init(0, 0, object_point.z, 1));
-	normal = v_normalize(cylinder_base_to_point);
-	world_normal = mv_mult(m_transpose(inverse_transform), normal);
-	normal = v_normalize(world_normal);
+	pc = v_sub(world_point, object->pos);
+	normal = v_sub(pc, v_scalar(object->shape.cylinder.axis_vector, v_dot(pc, object->shape.cylinder.axis_vector)));
 	free_matrix(inverse_transform);
 	return (normal);
 }
+
+// t_vector	compute_cylinder(t_object	*object, t_vector world_point)
+// {
+// 	double		**inverse_transform;
+// 	t_vector	cylinder_base_to_point;
+// 	t_vector	world_normal;
+// 	t_vector	object_point;
+// 	t_vector	normal;
+
+// 	inverse_transform = m_inverse(object->transformation_matrix);
+// 	object_point = mv_mult(inverse_transform, world_point);
+
+// 	cylinder_base_to_point = v_sub(object_point,
+// 			v_init(0, 0, object_point.z, 1));
+
+// 	normal = v_normalize(cylinder_base_to_point);
+// 	world_normal = mv_mult(m_transpose(inverse_transform), normal);
+// 	normal = v_normalize(world_normal);
+// 	free_matrix(inverse_transform);
+// 	return (normal);
+// }
 
 t_vector	compute_normal(t_intersections *intersection)
 {
